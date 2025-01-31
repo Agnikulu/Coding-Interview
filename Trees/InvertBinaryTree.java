4f2cde99dd61edc0a8a04ce68357e388e9dc08be
agnikulu <agnikulu@gmail.com>
2025-01-31 13:39:21 -0500

    Update code

--- Trees/InvertBinaryTree.java
+++ Trees/InvertBinaryTree.java
@@ -1,4 +1,17 @@
 public class InvertBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
